561a839719a679cbf078edc4519c566dc5dad978
Michael Avila <t3hh00d@gmail.com>
2013-03-07 23:18:21 -0800

    add test for process_view for redirect to login

--- stronghold/tests/middleware.py
+++ stronghold/tests/middleware.py
@@ -1,5 +1,11 @@
+import mock
+
+from stronghold.middleware import LoginRequiredMiddleware
+
 from django.core.urlresolvers import reverse
+from django.http import HttpResponse
 from django.test import TestCase
+from django.test.client import RequestFactory
 
 
 class StrongholdMiddlewareTestCase(TestCase):
@@ -15,4 +21,21 @@ class StrongholdMiddlewareTestCase(TestCase):
 
     def test_private_view_is_private(self):
         response = self.client.get(self.private_url)
+        self.assertEqual(response.status_code, 302)
+
+
+class LoginRequiredMiddlewareTests(TestCase):
+
+    def test_redirects_to_login_when_not_authenticated(self):
+        request = RequestFactory().get('/')
+        request.user = mock.Mock()
+        request.user.is_authenticated.return_value = False
+
+        response = LoginRequiredMiddleware().process_view(
+            request=request,
+            view_func=HttpResponse,
+            view_args=[],
+            view_kwargs={}
+        )
+
         self.assertEqual(response.status_code, 302)
