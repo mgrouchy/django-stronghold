17a99c97d1bfff8ada3ec27f967fb30a902de266
Michael Avila <t3hh00d@gmail.com>
2013-03-07 21:22:13 -0800

    clean up existing tests
    Mostly just getting rid of things that don't need to be here. Other small
    items are changes to be more PEP8 friendly.

--- stronghold/tests/middleware.py
+++ stronghold/tests/middleware.py
@@ -1,21 +1,18 @@
 from django.core.urlresolvers import reverse
-
-from django.utils import unittest
-from django.test.client import Client
+from django.test import TestCase
 
 
-class StrongholdMiddlewareTestCase(unittest.TestCase):
+class StrongholdMiddlewareTestCase(TestCase):
+
     @classmethod
     def setUpClass(cls):
         cls.public_url = reverse("public_view")
         cls.private_url = reverse("protected_view")
 
     def test_public_view_is_public(self):
-        client = Client()
-        response = client.get(self.public_url)
+        response = self.client.get(self.public_url)
         self.assertEqual(response.status_code, 200)
 
     def test_private_view_is_private(self):
-        client = Client()
-        response = client.get(self.private_url)
+        response = self.client.get(self.private_url)
         self.assertEqual(response.status_code, 302)
