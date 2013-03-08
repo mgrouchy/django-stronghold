47536c04ea5f2f6477cc8e30017ac1a8d0fc6d14
Michael Avila <t3hh00d@gmail.com>
2013-03-08 00:28:00 -0800

    remove integration tests; all unit tests cover integration

--- stronghold/tests/middleware.py
+++ stronghold/tests/middleware.py
@@ -4,26 +4,9 @@ import re
 from stronghold import conf
 from stronghold.middleware import LoginRequiredMiddleware
 
-from django.core.urlresolvers import reverse
 from django.http import HttpResponse
 from django.test import TestCase
 from django.test.client import RequestFactory
-
-
-class StrongholdMiddlewareTestCase(TestCase):
-
-    @classmethod
-    def setUpClass(cls):
-        cls.public_url = reverse("public_view")
-        cls.private_url = reverse("protected_view")
-
-    def test_public_view_is_public(self):
-        response = self.client.get(self.public_url)
-        self.assertEqual(response.status_code, 200)
-
-    def test_private_view_is_private(self):
-        response = self.client.get(self.private_url)
-        self.assertEqual(response.status_code, 302)
 
 
 class LoginRequiredMiddlewareTests(TestCase):
