7d7c049057cce02b12ab435b770bf3e9d9d596aa
Mike Grouchy <mgrouchy@gmail.com>
2013-03-09 20:40:40 -0500

    version bump to fix a typo

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ test_dependencies = ['django>1.3.1']
 
 setup(
     name='django-stronghold',
-    version='0.1.0',
+    version='0.1.1',
     description='Get inside your stronghold and make all your Django views default login_required',
     url='https://github.com/mgrouchy/django-stronghold',
     author='Mike Grouchy',
