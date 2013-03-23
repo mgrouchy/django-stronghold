104600faef614e18ecfca7c2ded11936781ce686
Mike Grouchy <mgrouchy@gmail.com>
2013-03-22 22:27:39 -0400

    default html for docs

---
+++ docs/_build/html/searchindex.js
@@ -0,0 +1,1 @@
+Search.setIndex({objects:{},terms:{stronghold:0,index:0,search:0,welcom:0,modul:0,indic:0,django:0,content:0,tabl:0,document:0,page:0},objtypes:{},titles:["Welcome to django-stronghold&#8217;s documentation!"],objnames:{},filenames:["index"]})
