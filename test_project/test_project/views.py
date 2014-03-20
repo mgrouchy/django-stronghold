fc5c9e3fe5da7e421315653e744570ebafd1faa0
Ludwik Trammer <ludwik@gmail.com>
2014-03-20 19:44:55 +0100

    Additional testing of public views

--- test_project/test_project/views.py
+++ test_project/test_project/views.py
@@ -6,16 +6,38 @@ from stronghold.decorators import public
 
 
 class ProtectedView(View):
-    """A view we want to be private"""
+    """
+    A view we want to be private
+    """
     def get(self, request, *args, **kwargs):
         return HttpResponse("ProtectedView")
 
 
-class PublicView(View):
-    """ A view we want to be public"""
+class PublicViewDecorator(View):
+    """
+    A view made public using decorator
+    """
     @method_decorator(public)
     def dispatch(self, *args, **kwargs):
-        return super(PublicView, self).dispatch(*args, **kwargs)
+        return super(PublicViewDecorator, self).dispatch(*args, **kwargs)
 
     def get(self, request, *args, **kwargs):
-        return HttpResponse("PublicView")
+        return HttpResponse("PublicViewDecorator")
+
+
+class PublicViewURL(View):
+    """
+    A view made public using the
+    STRONGHOLD_PUBLIC_URLS setting
+    """
+    def get(self, request, *args, **kwargs):
+        return HttpResponse("PublicViewURL")
+
+
+class PublicViewNamedURL(View):
+    """
+    A view made public using the
+    STRONGHOLD_PUBLIC_NAMED_URLS setting
+    """
+    def get(self, request, *args, **kwargs):
+        return HttpResponse("PublicViewNamedURL")
