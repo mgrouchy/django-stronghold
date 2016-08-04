ca8db5e46fa3e6c767b4842c4ab7cb823cef2fde
James Valleroy <jvalleroy@mailbox.org>
2016-08-04 18:48:58 -0400

    Add support for Django 1.10

--- test_project/test_project/urls.py
+++ test_project/test_project/urls.py
@@ -1,9 +1,8 @@
-from django.conf.urls import patterns, url
+from django.conf.urls import url
 from . import views
 
 
-urlpatterns = patterns(
-    '',
+urlpatterns = [
     url(r'^protected/$', views.ProtectedView.as_view(), name="protected_view"),
     url(r'^public/$', views.PublicView.as_view(), name="public_view"),
-)
+]
