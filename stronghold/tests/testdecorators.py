c8c9d5d04297731173685a08a95acdbc9841b145
cw0102 <18674410+cw0102@users.noreply.github.com>
2019-04-15 13:09:09 -0400

    New approach to preserve partials Also update travis.yml

--- stronghold/tests/testdecorators.py
+++ stronghold/tests/testdecorators.py
@@ -7,6 +7,7 @@ if django.VERSION[:2] < (1, 9):
     from django.utils import unittest
 else:
     import unittest
+from django.utils.decorators import method_decorator
 
 
 class StrongholdDecoratorTests(unittest.TestCase):
@@ -37,3 +38,23 @@ class StrongholdDecoratorTests(unittest.TestCase):
         decorators.public(partial)
 
         self.assertTrue(function.STRONGHOLD_IS_PUBLIC)
+
+    def test_public_decorator_works_with_method_decorator(self):
+        class TestClass:
+            @method_decorator(decorators.public)
+            def function(self):
+                pass
+
+        self.assertTrue(TestClass.function.STRONGHOLD_IS_PUBLIC)
+
+    def test_public_method_decorator_works_with_partials(self):
+        def partial_decorator(func):
+            return functools.partial(func)
+
+        class TestClass:
+            @method_decorator(decorators.public)
+            @method_decorator(partial_decorator)
+            def function(self):
+                pass
+        
+        self.assertTrue(TestClass.function.STRONGHOLD_IS_PUBLIC)
