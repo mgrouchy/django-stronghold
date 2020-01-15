d601baee0dedace946b30de633b853c39e6c6806
Mike Grouchy <mgrouchy@gmail.com>
2020-01-14 23:02:48 -0500

    Clean up for new version
    * Python 3.7 Support testing. 
    * Bump Jinja (Re security issue).

--- setup.py
+++ setup.py
@@ -8,33 +8,31 @@ except ImportError:
 
 
 dependencies = []
-test_dependencies = ['django>1.8.0']
+test_dependencies = ["django>1.8.0"]
 
 setup(
-    name='django-stronghold',
-    version='0.3.0',
-    description='Get inside your stronghold and make all your Django views default login_required',
-    url='https://github.com/mgrouchy/django-stronghold',
-    author='Mike Grouchy',
+    name="django-stronghold",
+    version="0.3.0",
+    description="Get inside your stronghold and make all your Django views default login_required",
+    url="https://github.com/mgrouchy/django-stronghold",
+    author="Mike Grouchy",
     author_email="mgrouchy@gmail.com",
-    packages=[
-        'stronghold',
-        'stronghold.tests',
-    ],
-    license='MIT license',
+    packages=["stronghold", "stronghold.tests",],
+    license="MIT license",
     install_requires=dependencies,
     tests_require=test_dependencies,
-    long_description=open('README.rst').read(),
+    long_description=open("README.rst").read(),
     classifiers=[
-        'Development Status :: 5 - Production/Stable',
-        'Intended Audience :: Developers',
-        'Natural Language :: English',
-        'License :: OSI Approved :: MIT License',
-        'Programming Language :: Python',
-        'Programming Language :: Python :: 2.7',
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.4',
-        'Programming Language :: Python :: 3.5',
-        'Programming Language :: Python :: 3.6',
+        "Development Status :: 5 - Production/Stable",
+        "Intended Audience :: Developers",
+        "Natural Language :: English",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.4",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
     ],
 )
--- stronghold/middleware.py
+++ stronghold/middleware.py
@@ -23,12 +23,14 @@ class LoginRequiredMiddleware(MiddlewareMixin):
     def __init__(self, *args, **kwargs):
         if MiddlewareMixin != object:
             super(LoginRequiredMiddleware, self).__init__(*args, **kwargs)
-        self.public_view_urls = getattr(conf, 'STRONGHOLD_PUBLIC_URLS', ())
+        self.public_view_urls = getattr(conf, "STRONGHOLD_PUBLIC_URLS", ())
 
     def process_view(self, request, view_func, view_args, view_kwargs):
-        if conf.STRONGHOLD_USER_TEST_FUNC(request.user) \
-                or utils.is_view_func_public(view_func) \
-                or self.is_public_url(request.path_info):
+        if (
+            conf.STRONGHOLD_USER_TEST_FUNC(request.user)
+            or utils.is_view_func_public(view_func)
+            or self.is_public_url(request.path_info)
+        ):
             return None
 
         decorator = user_passes_test(conf.STRONGHOLD_USER_TEST_FUNC)
